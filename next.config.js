3c5590ae0107957126128463ae2c0f1fcb9b91be
L0giXX <marc.mueller04@gmail.com>
2023-08-08 22:18:51 +0200

    prod next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const rehypePrettyCode = require("rehype-pretty-code");
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: "github-dark",
+};
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
+});
+module.exports = withMDX(nextConfig);
