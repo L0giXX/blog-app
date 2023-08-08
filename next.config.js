cf0538f9c295b0097e689e684337235e2c754a63
L0giXX <marc.mueller04@gmail.com>
2023-08-08 20:06:01 +0200

    prod

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-};
+const nextConfig = {};
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
