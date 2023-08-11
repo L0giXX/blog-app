fd729f2f38cbc809abdc00ac1538f1f3d15cd1b2
L0giXX <marc.mueller04@gmail.com>
2023-08-11 23:08:32 +0200

    prod change

--- src/app/api/views/route.ts
+++ src/app/api/views/route.ts
@@ -4,6 +4,9 @@ import { prisma } from "@lib/prisma";
 export async function GET(request: Request) {
   const origin = request.headers.get("origin");
   const posts = await prisma.post.findMany();
+  if (!posts) {
+    return NextResponse.json(null);
+  }
   return NextResponse.json(
     { posts },
     {
