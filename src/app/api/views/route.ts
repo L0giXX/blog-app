fe0a3111e5fd9d414d605c94ba721424c185731f
L0giXX <marc.mueller04@gmail.com>
2023-08-11 22:57:30 +0200

    trying out some routes changes

--- src/app/api/views/route.ts
+++ src/app/api/views/route.ts
@@ -1,11 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@lib/prisma";
 
-export async function GET() {
-  const posts = await prisma.post.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-  return NextResponse.json(posts);
+export async function GET(request: Request) {
+  const origin = request.headers.get("origin");
+  const posts = await prisma.post.findMany();
+  return NextResponse.json({ posts });
 }
---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { TailSpin } from "react-loader-spinner";
+
+export default function Loading() {
+  return (
+    <div className="mx-auto max-w-[640px]">
+      <TailSpin
+        height="40"
+        width="40"
+        color="#FFFFFF"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+}
