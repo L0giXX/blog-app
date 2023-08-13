7b4ba59692f3bfe0a1897a191ddcf057b664bad9
L0giXX <marc.mueller04@gmail.com>
2023-08-13 23:12:47 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
+const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
-import { withContentlayer } from "next-contentlayer";
 const nextConfig = {};
 
 module.exports = withContentlayer(nextConfig);
