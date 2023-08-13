8d484c41145da86e8b70ee72d7826243482441b8
L0giXX <marc.mueller04@gmail.com>
2023-08-13 23:09:36 +0200

    prod changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+import { withContentlayer } from "next-contentlayer";
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
