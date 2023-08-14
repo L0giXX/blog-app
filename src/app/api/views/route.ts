728ed1160fa3d0923bb972ad14dc036e6f7677d9
L0giXX <marc.mueller04@gmail.com>
2023-08-14 23:56:07 +0200

    add force dynamic

--- src/app/api/views/route.ts
+++ src/app/api/views/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@lib/prisma";
 
-export async function GET(request: Request) {
-  const posts = await prisma.post.findMany();
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      views: true,
+    },
+  });
   return NextResponse.json({ posts });
 }
